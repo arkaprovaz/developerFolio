07685c79d802472dba60ab85fcfdab292fd926fd
Arkaprova Deb <arkaprova.deb97@gmail.com>
2020-08-10 05:21:42 +0530

    Updates

---
+++ precache-manifest.af30b2e1cce5841107e068db88bdd9b6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4080f867ef89a8ecaa87a02ee85a839c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4d1d6bdb0b5c791fb6b",
+    "url": "/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "236f918dea9ca32887a5",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "40a3e0767b81ebd6b5b4",
+    "url": "/static/css/main.7513e02b.chunk.css"
+  },
+  {
+    "revision": "50f9b21ffdcdd12ef056",
+    "url": "/static/js/2.7d5fd23c.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/static/js/2.7d5fd23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d1d6bdb0b5c791fb6b",
+    "url": "/static/js/3.b9fb1b5b.chunk.js"
+  },
+  {
+    "revision": "236f918dea9ca32887a5",
+    "url": "/static/js/4.f44dcdcf.chunk.js"
+  },
+  {
+    "revision": "40a3e0767b81ebd6b5b4",
+    "url": "/static/js/main.cbb9b714.chunk.js"
+  },
+  {
+    "revision": "8435d9591f9d449bfbe4",
+    "url": "/static/js/runtime-main.4ae9654a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "01235145219389240beb916dd9e3191c",
+    "url": "/static/media/blackspektro.01235145.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "d97c3efbf0e6b06fa1268ebc62dfa5b8",
+    "url": "/static/media/gdg.d97c3efb.png"
+  },
+  {
+    "revision": "6b914c0c08c8a3cd1c6b81af58a8437f",
+    "url": "/static/media/intel.6b914c0c.png"
+  },
+  {
+    "revision": "e6d451a1c8f2ea206bb576691a14ee8a",
+    "url": "/static/media/isi.e6d451a1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "43218ba153c2eca5c07ad312ce39f875",
+    "url": "/static/media/openvino.43218ba1.png"
+  },
+  {
+    "revision": "d49ee4c76bec54cb41d57462e1682724",
+    "url": "/static/media/p360.d49ee4c7.png"
+  },
+  {
+    "revision": "ca1fb5befb03bef7423c5d6ae6dd9813",
+    "url": "/static/media/sih2020.ca1fb5be.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "f328c1cc7632f68f547d228f597cba26",
+    "url": "/static/media/techniche.f328c1cc.png"
+  }
+]);
