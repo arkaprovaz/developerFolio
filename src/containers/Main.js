04971db4e34dd25352d31408ee56c4c87b302e0e
Arkaprova Deb <arkaprova.deb97@gmail.com>
2020-08-10 03:45:06 +0530

    Removed podcast

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -31,7 +31,6 @@ export default class Main extends Component {
         <Blogs />
         <Talks />
         <Twitter />
-        <Podcast />
         <Profile />
         <Footer />
         <Top />
